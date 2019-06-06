a9d8fdad6eea7341201244b35a0babca9274016d
Hannah Norish <hannah@213a.ca>
2019-06-06 15:00:09 -0400

    footer components added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,111 +2,54 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mailTo: 'info@globalmeshlabs.com',
+      linkList: ['www.twitter.ca', 'www.twitter.ca', 'www.twitter.ca', 'www.twitter.ca', 'www.twitter.ca'],
+      subLinks: ['privacy', 'Terms of service', 'Open source']
+    }
+  }
   render() {
+    console.log('this.state: ', this.state)
+    let links = this.state.linkList.map((link) =>
+      <a href="{link}">{link}</a>
+    )
+    let subLinks = this.state.subLinks.map((sublink) =>
+      <a href="{sublink}">{sublink}</a>
+    )
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
+      <footer className="cFooter has-background-black has-text-white-ter">
+        <div classname="cFooter-info">
+          <h2 className="cFooter-title">Stay Connected</h2>
+          <p className="cFooter-subtitle">Receive updates from Global Mesh Labs.</p>
+          <form name="myform" action="handle-data.pl">
+            <input type="email" placeholder="YOUR EMAIL"></input>
+            <input type="submit" value="SIGN UP" />
+          </form>
+          <p className="cFooter-subtext">Contact Us: <a href="mailto:{this.state.mailTo}">{this.state.mailTo}</a></p>
+          <div className="cFooter-linkList">
+            {links}
           </div>
         </div>
+        <div classname="cFooter-subsection">
+          <div classname="cFooter-subsection-inner">
+            <div className="content has-text-centered">
+              <img
+                src={logo}
+                alt="Kaldi"
+                style={{ width: '14em', height: '10em' }}
+              />
+            </div>
+            <div className="cFooter-sublinkList">
+              {subLinks}
+            </div>
+          </div>
+          <p className="cFooter-footnote">Global Mesh Labs, LLC</p>
+        </div>
+
       </footer>
     )
   }
