0518f377855a9baabacf483b8424f93247f21481
Hannah Norish <hannah@213a.ca>
2019-06-07 17:45:09 -0400

    links added

--- src/components/HomeSlider.js
+++ src/components/HomeSlider.js
@@ -4,21 +4,20 @@ import infographic from '../img/infographic.svg';
 class HomeSlider extends React.Component {
   render() {
     const { data } = this.props
-
     return (
       <div  className="cHomeSlider">
         <div className="cHomeSlider-inner">
           <div className="cHomeSlider-text">
-            <div className="cHomeSlider-title"> 
+            <div className="cHomeSlider-title">
               {this.props.children[0]}
             </div>
-            <div className="cHomeSlider-subtitle"> 
+            <div className="cHomeSlider-subtitle">
               {this.props.children[1]}
             </div>
             <div>SLIDER GOES HERE</div>
             <div className="cHomeSlider-buttons">
-              <a className="cHomeSlider-btn" href="#">Download Whitepaper</a>
-              <a className="cHomeSlider-btn" href="#">View gitbook</a>
+              <a className="cHomeSlider-btn" href="https://app.gitbook.com/@global-mesh-labs/s/lot49/">Download Whitepaper</a>
+              <a className="cHomeSlider-btn" href="https://app.gitbook.com/@global-mesh-labs/s/lot49/">View gitbook</a>
             </div>
           </div>
         <img src={infographic} alt="Global Mesh Labs" />
