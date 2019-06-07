4e30068730f4398b5cb5c8ca152830fda9e6382f
Hannah Norish <hannah@213a.ca>
2019-06-07 16:50:56 -0400

    looking good

--- src/components/HomeSlider.js
+++ src/components/HomeSlider.js
@@ -8,11 +8,18 @@ class HomeSlider extends React.Component {
     return (
       <div  className="cHomeSlider">
         <div className="cHomeSlider-inner">
-          <div className="cHomeSlider-title"> 
-            {this.props.children[0]}
-          </div>
-          <div className="cHomeSlider-subtitle"> 
-            {this.props.children[1]}
+          <div className="cHomeSlider-text">
+            <div className="cHomeSlider-title"> 
+              {this.props.children[0]}
+            </div>
+            <div className="cHomeSlider-subtitle"> 
+              {this.props.children[1]}
+            </div>
+            <div>SLIDER GOES HERE</div>
+            <div className="cHomeSlider-buttons">
+              <a className="cHomeSlider-btn" href="#">Download Whitepaper</a>
+              <a className="cHomeSlider-btn" href="#">View gitbook</a>
+            </div>
           </div>
         <img src={infographic} alt="Global Mesh Labs" />
         </div>
