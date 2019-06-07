384116968bbc6b9d7891e2f35a9e766f663f377b
Hannah Norish <hannah@213a.ca>
2019-06-07 12:39:50 -0400

    mobile looking better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = class extends React.Component {
       linkURL = Object.values(link)
       linkLabel = Object.keys(link);
         return <a className="cFooter-link white" href="{linkURL}">{linkLabel}
-          <svg className="chevron-right" viewBox="0 0 100 100" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+          <svg className="chevron-right" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
             <path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"/>
           </svg>
         </a>
