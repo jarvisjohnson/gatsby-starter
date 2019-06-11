275c6fcbdfd2d2e90bca891b6e8226f6d693f2e1
Alexandre Leclair <alexandre.leclair@zloche.net>
2019-06-11 11:04:36 -0400

    New OG image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -43,7 +43,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content="/img/og-image.jpg" />
+        <meta property="og:image" content="/img/og-image-new.jpg" />
         <link rel="stylesheet" href="https://use.typekit.net/zpz2esi.css" />
 
 
